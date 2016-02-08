6021515c87761ddcffeeff823e8c09ce309b8308
studyOreshkin <studyOreshkin@gmail.com>
2016-02-08 21:47:46 +0300

    add trace, clarify

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-var koa = require('koa');
-var app = koa();
+'use strict';
+
+require('trace');
+require('clarify');
+
+let koa = require('koa');
+let app = koa();
 
 app.use(function *(){
     this.body = 'Hello World';
