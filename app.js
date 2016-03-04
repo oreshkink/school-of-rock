29e290555dc14d8a9d2051f3fb0ccc4930c13cf4
studyOreshkin <studyOreshkin@gmail.com>
2016-03-04 21:17:34 +0300

    подружился с jade, koa-router

--- app.js
+++ app.js
@@ -4,12 +4,61 @@ require('trace');
 require('clarify');
 
 let koa = require('koa');
+let koa_router = require('koa-router');
 let mongoose = require('mongoose');
 let jade = require('jade');
 
 let app = koa();
+let router = koa_router();
 
-app.use(function*() {
+router
+    // Главная страница
+    .get('/', function *(next) {
+        this.body = 'Hello World!';
+    })
+    // Направления обучения
+    .get('/classes', function *(next) {
+        this.body = jade.renderFile('templates/hello.jade');
+    })
+    .get('/classes/:id', function *(next) {
+        this.body = 'Classes/1';
+    })
+    // Преподаватели
+    .get('/teachers', function *(next) {
+        this.body = 'Teachers';
+    })
+    .get('/teachers/:id', function *(next) {
+        this.body = 'Teachers';
+    });
+
+app
+    .use(views(__dirname + '/templates', { map: { html: 'jade' } }))
+    .use(router.routes())
+    .use(router.allowedMethods());
+
+
+app.use(function *(next) {
+    try {
+        yield next;
+    } catch (err) {
+        this.status = 500;
+        this.body = err.message;
+        this.app.emit("error", err, this);
+    }
+});
+
+// error thrower
+app.use(function *(next) {
+    if (this.url === "/_err") {
+        throw new Error("Send Error");
+    }
+
+    yield next;
+});
+
+
+app.on('error', function(err){
+    console.error(err.stack);
 });
 
 app.listen(3000);
